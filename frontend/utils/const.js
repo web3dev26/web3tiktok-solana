abb05859aac82b96f260073a55293e3d6cec9ecc
web3dev26 <106200773+web3dev26@users.noreply.github.com>
2022-05-30 02:51:08 +0300

    Update const.js

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -5,7 +5,7 @@ import tiktok from './tiktok_clone.json'
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
 export const TIKTOK_PROGRAM_ID = new PublicKey(
-    "84ycHNkVrLem741fXUGNTyufi2qNGMbLVcwkZDZmyNwb"
+    "GiMxGpXwqL9HtcNNZhbB1iZHtxJEq56YzL4NzqA5Ui5z"
 )
 
 export const TIKTOK_IDL = tiktok
